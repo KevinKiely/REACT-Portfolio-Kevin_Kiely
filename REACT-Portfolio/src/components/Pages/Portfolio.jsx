19555ffce146c18f4675b595b335614ecb568ac1
KevinKiely <Kevinkiely762@gmail.com>
2024-06-09 23:04:32 -0400

    Finishing touches, contact component, added URLs for all projects

--- REACT-Portfolio/src/components/Pages/Portfolio.jsx
+++ REACT-Portfolio/src/components/Pages/Portfolio.jsx
@@ -1,8 +1,50 @@
+
+
+
+
 export default function Portfolio() {
     return (
-      <div>
-        <h1> THIS IS THE PORTFOLIO SECTION </h1>
+      <div className="container-fluid">
+        <div className="row align-items-center justify-content-center">
+
+
+          <div class="col-5 border m-3 p-2 d-flex flex-column align-items-center">
+            <h2> Date Advisor</h2>
+            <a rel="Github" href="https://github.com/KevinKiely/Date-Advisor"> Github Repository </a>
+            <a rel="Live-Site" href="https://kevinkiely.github.io/Date-Advisor/"> Live Application </a>
+          </div>
+
+
+          <div class="col-5 border m-3 p-2 d-flex flex-column align-items-center">
+            <h2> KBI Education</h2>
+            <a rel="Github" href="https://github.com/KevinKiely/Education-Management-Application"> Github Repository </a>
+            <a rel="Live-Site" href="https://kbieducation-6239e4e5ab51.herokuapp.com/"> Live Application </a>
+          </div>
+
+
+          <div class="col-5 border m-3 p-2 d-flex flex-column align-items-center">
+            <h2> Gighub</h2>
+            <a rel="Github" href="https://github.com/KevinKiely/Gighub"> Github Repository </a>
+            <a rel="Live-Site" href="https://kevinkiely.github.io/Date-Advisor/"> Live Application </a>
+          </div>
+
+
+          <div class="col-5 border m-3 p-2 d-flex flex-column align-items-center">
+            <h2> COMING SOON</h2>
+            
+          </div>
+
+
+
+
+
+
+
+        </div>
+    
       </div>
     );
   }
+
+
   
